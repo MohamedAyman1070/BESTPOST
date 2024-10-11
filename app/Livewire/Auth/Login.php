3c320606ac2aaadcf041c96c6530f2dcf389175b
APPPRAXICAN <unknownworlds990@gmail.com>
2024-10-11 23:55:05 +0300

    error in login cateched but not solved

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,25 +2,31 @@
 
 namespace App\Livewire\Auth;
 
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 
 class Login extends Component
 {
-    public $email ;
+    public $email;
     public $password;
 
     protected $rules = [
-        'email' =>'required|email',
-        'password' =>'required|min:6',
+        'email' => 'required|email',
+        'password' => 'required|min:6',
     ];
-    
-    public function login(){
+
+    public function login()
+    {
         $attrs = $this->validate();
 
-        if( !Auth::attempt($attrs) ){
-            throw ValidationException::withMessages(['password' => 'Your login credentials dont match an account in our system']);
+        try {
+            if (!Auth::attempt($attrs)) {
+                throw ValidationException::withMessages(['password' => 'Your login credentials dont match an account in our system']);
+            }
+        } catch (Exception $e) {
+            throw ValidationException::withMessages(['password' => $e->getMessage()]);
         }
         session()->regenerate();
         return redirect('/');
