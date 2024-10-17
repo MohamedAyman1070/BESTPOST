1e63af2378a4242ebbb90c38c0760b7963ea4b27
APPPRAXICAN <unknownworlds990@gmail.com>
2024-10-17 07:01:50 +0300

    test

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -10,37 +10,38 @@ use Laravel\Socialite\Facades\Socialite;
 
 class GoogleAuthController extends Controller
 {
-    public function redirect(){
+    public function redirect()
+    {
         return Socialite::driver('google')->redirect();
     }
 
-    public function callbackGoogle(){
-        try{
+    public function callbackGoogle()
+    {
+        try {
             $google_user =  Socialite::driver('google')->user();
-            $user = User::where('google_id'  , $google_user->getId())->first();
-            if(!$user){
+            $user = User::where('google_id', $google_user->getId())->first();
+            if (!$user) {
 
                 $color = array();
- 
-                $color[] = (string)rand(50 ,255);
-                $color[] = ','.(string)rand(50 ,255);
-                $color[] = ','.(string)rand(50 ,255);
-            
+
+                $color[] = (string)rand(50, 255);
+                $color[] = ',' . (string)rand(50, 255);
+                $color[] = ',' . (string)rand(50, 255);
+
                 $newUser = User::create([
                     'google_id' => $google_user->getId(),
                     'name' => $google_user->getName(),
                     'email' => $google_user->getEmail(),
                     'background_color' => implode($color),
                 ]);
-                
+
                 Auth::login($newUser);
                 return redirect('/');
             }
             Auth::login($user);
             return redirect('/');
-
-        }catch(Exception $e){
-            dd('ERR : ',$e->getMessage());
+        } catch (Exception $e) {
+            dd('ERR : ', $e->getMessage());
         }
     }
 }
