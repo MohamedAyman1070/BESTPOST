88c2a6ee556d7ed3a047b1c7ea00c522c54670dd
APPPRAXICAN <unknownworlds990@gmail.com>
2024-10-19 04:14:07 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@ use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 Route::get('/login', function () {
-    dd(sys_get_temp_dir());
-
     return view('Auth.login');
 })->name('login');
 
